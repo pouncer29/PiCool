c7401c338af2de323cd21c098c0aa1cda81433e8
fbl773 <fbl773@mail.usask.ca>
2023-01-26 11:58:02 -0600

    Methodized the assignment process, All tests passing

--- src/conf_loader.c
+++ src/conf_loader.c
@@ -2,10 +2,65 @@
 
 CONFIG* fan_config = NULL;
 
+/**
+ * Synopsis: Assigns the value within confline to the appropriate my_config field
+ * precondition: conf_line is a line of input 
+ * postcondition: my_config has been assigned a new value
+ * return: 0 on success 1 on failed scan, 2 on bad keyword len, 
+ */
+int assign_values(char* conf_line, CONFIG* my_config){
+
+	//Check to see if this line contains more than one character
+	if(strlen(conf_line) > 1){
+			
+		char keyword[MAX_KEYWORD_LEN]; //Buffer for the keyword only
+		char* value; //Buffer for the keyword only
+
+		//Pull out keyword
+		int scan_ret = sscanf(conf_line,"%s",keyword);
+		if(scan_ret > 1){
+			errx(1,"FAILED TO SCAN VALUE\n");
+			return 1;
+		}
+
+		//check keyword len
+		if(strlen(keyword) >= MAX_KEYWORD_LEN){
+			errx(1,strcat(
+				"INVALID CONF FILE: MAX KEYWORD LEN EXCEEDED:\n"
+				,keyword)
+			);
+			return 2;
+		}	
+
+		//Pull out value (everything after the space)
+		value = strchr(conf_line,' ') + 1; //+1 advances past the space
+
+
+		//Read in the appropriate value  (CANDIATE FOR NEW FN())
+		if(strcmp(keyword,"FAN_PIN") == 0){
+			sscanf(value,"%hhi",&(my_config->fan_pin));
+		} else if (strcmp(keyword,"POLL_TIME") == 0){
+			sscanf(value,"%hhi",&(my_config->poll_time));
+		} else if (strcmp(keyword,"ACTIVE_TEMP") == 0){
+			sscanf(value,"%f",&(my_config->active_thresh));
+		} //Else, we don't care what it is.
+	}
+	return 0;
+}
+
+/**
+ * Synopsis: Reads the config file and assigns values to a newly created 
+ * 	     config file
+ * Preconditions: conf_file passed exists
+ * Postconditions: New config struct is created and assigned values
+ * Return: Pointer to a new config object
+ */
 CONFIG* load_config(char* config_path){
 
 	//Load up a new config struct
 	CONFIG* my_config = (CONFIG*) malloc(sizeof(CONFIG));
+
+	//TODO: Assign Default values maybe?
 
 	//Open the file	
 	FILE* conf_file = fopen(config_path,"r");
@@ -15,49 +70,11 @@ CONFIG* load_config(char* config_path){
 		return NULL;
 	}
 
-	
-
-	//Collect the values
-	int scan_ret = 0; 		//Allows us to examine the scan return.
 	char conf_line[20];		//Holds a full line of input
 
-	//Look at the line of the file
+	//Loop through file, and assign values
 	while(fgets(conf_line,20,conf_file) != NULL){
-			
-		char keyword[MAX_KEYWORD_LEN]; //Buffer for the keyword only
-		char* value; //Buffer for the keyword only
-
-		//Pull out keyword
-		scan_ret = sscanf(conf_line,"%s",keyword);
-		if(scan_ret > 1){
-			errx(1,"FAILED TO SCAN VALUE\n");
-			return NULL;
-		}
-
-		//check keyword len
-		if(strlen(keyword) >= MAX_KEYWORD_LEN){
-			errx(1,strcat(
-				"INVALID CONF FILE: MAX KEYWORD LEN EXCEEDED:\n"
-				,keyword)
-			);
-			return NULL;
-		}	
-
-		//Pull out value (everything after the space)
-		value = strchr(conf_line,' ') + 1; //+1 advances past the space
-
-		
-
-		//Read in the appropriate balue
-		if(strcmp(keyword,"FAN_PIN") == 0){
-			sscanf(value,"%hhi",&(my_config->fan_pin));
-		} else if (strcmp(keyword,"POLL_TIME") == 0){
-			sscanf(value,"%hhi",&(my_config->poll_time));
-		} else if (strcmp(keyword,"ACTIVE_TEMP") == 0){
-			sscanf(value,"%f",&(my_config->active_thresh));
-		} else {
-			char* err_msg = strcat("Unrecognized value: ",keyword);
-			errx(1, strcat(err_msg,"\n"));
+		if(assign_values(conf_line,my_config) != 0){
 			return NULL;
 		}
 	}
@@ -70,6 +87,7 @@ CONFIG* load_config(char* config_path){
 		return NULL;
 	}
 
+	//my_config must now contain all that it had.
 	return my_config;
 }
 
