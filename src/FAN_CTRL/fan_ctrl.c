36a5804843dcf0f2bf5541b4a7bea6440b9525bc
fbl773 <fbl773@mail.usask.ca>
2023-02-13 14:15:26 -0600

    errors on the init process, but making progress

--- src/FAN_CTRL/fan_ctrl.c
+++ src/FAN_CTRL/fan_ctrl.c
@@ -1,8 +1,54 @@
 #include <fan_ctrl.h>
 
-
+/*
+initialize_GPIO()
+	info: uses 2.1 to get fan pin and initializes the appropriate sys files
+		to enable GPIO on that pin
+	synopsis:
+		write pin# to /sys/class/gpio/export (to initialize)
+		write 'out' to /sys/class/gpio/gpio<PIN#>/direction (to set output)
+	params: none
+	return: 1 on failure, 0 on success
+*/
 uint8_t initialize_GPIO(){
-	return 255;
+
+	//TODO: Check to see if everything has been configured first maybe....
+	
+	//Step ZERO: load the config file
+	get_config(NULL);
+
+	//Grab the string of the pin #
+	char pin_num_str[3];
+	uint8_t fan_pin = get_fan_pin();
+	sprintf(pin_num_str,"%hhi",fan_pin);
+
+	//Step ONE: write "PIN#" to GPIO_ROOT/export
+	FILE* export_file = fopen(GPIO_path_plus("/export"),"w");
+	if(NULL == export_file){	
+		return 1;
+	}
+	//otherwise, we are good
+	fprintf(export_file,"%s",pin_num_str);
+
+	//TODO: WARNING - DEPENDING ON HOW FAST THIS WORKS, gpioXX may not exist.
+	
+	//Step TWO: write "out" to GPIO_ROOT/gpio<PIN>/direction
+
+	//Load in our string
+	int pin_path_len = strlen("/gpioXX/direction") + 1;
+	char pin_path[pin_path_len];
+	sprintf(pin_path,"/gpio%s/direction",pin_num_str);
+	
+	//Open our pin file
+	FILE* fan_pin_file = fopen(GPIO_path_plus(pin_path),"w");
+	if(NULL == fan_pin_file){	
+		return 1;
+	}
+
+	//otherwise, we are good to set pin XX to output mode
+	fprintf(fan_pin_file,"out");
+
+	return 0;
 }
 
 uint8_t activate_fan(){
