41ec1b92894f1bab0e4a4b94b03acfb6082fe316
frankyb <frankyb@gurgi>
2023-03-09 14:20:38 -0600

    Reset the PICOOL makefile to send the executable to /bin, also removed the extra GPIO init that I added thinking I hadn't done that already on another branch

--- src/PICOOL/picool.c
+++ src/PICOOL/picool.c
@@ -64,12 +64,6 @@ int main(int argc, char* argv[]){
 		syslog(LOG_ERR,"ERROR: FAILED TO INIT GPIO %d",gpio_init);
 	}
 
-	int init_gpio_success = initialize_GPIO();
-	if(init_gpio_success != 0){
-		fprintf(stderr,"ERROR: FAILED TO INIT GPIO: %d",init_gpio_success);
-		return EXIT_FAILURE;
-	}
-
 	/** FOREVER */
 	//HOld on, TODO: Free the config file on exit
 	while(run == 1){
